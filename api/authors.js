2ce96d13eb35ec42dfe91c4dca4bafd09cefaeed
CourtneyATX <courtney.mcguire.tx@gmail.com>
2023-11-01 14:35:55 -0500

    added routes to books/authors

--- api/authors.js
+++ api/authors.js
@@ -1,30 +1,162 @@
 const router = require('express').Router();
 module.exports = router;
 
-router.get('/', (req, res) => {
+const prisma = require('../prisma');
+
     //returns array of authors
+router.get('/', async (req, res, next) => {
+    try {
+        const authors = await prisma.author.findMany();
+        res.json(authors);
+    } catch {
+        next();
+    }
 });
 
-router.post('/', (req, res) => {
     //creates a new author with info provided from request body
+router.post('/', async (req, res, next) => {
+    try {
+        const { name } = req.body;
+
+        if (!name) {
+            const error = {
+                status: 400,
+                message: `Author must have a name.`,
+            };
+
+            return next(error);
+        }
+        const author = await prisma.author.create({ data: { name }});
+            res.json(author);
+    } catch {
+        next();
+    }
 });
 
-router.get('/:id', (req, res) => {
     //gets a single author with specified id
+router.get('/:id', async (req, res, next) => {
+    try {
+        const id = +req.params.id;
+        const author = await prisma.author.findUnique({ where: { id }});
+
+        if (!author) {
+            return next({
+                status: 404,
+                message: `Could not find author with id ${id}.`,
+            });
+        }
+        res.json(author);
+    } catch {
+        next();
+    }
 });
 
-router.put('/:id', (req, res) => {
     //overwrites the author with info provided in req body
+router.put('/:id', async (req, res, next) => {
+    try {
+        const id = +req.params.id;
+
+        const authExists = await prisma.author.findUnique({ where: { id }});
+
+        if (!authExists) {
+            return next({
+                status: 404,
+                message: `No author with id ${id} exists.`,
+            });
+        }
+
+        const { name } = req.body;
+        if (!name) {
+            return next({
+                status: 400, 
+                message: `Author must have a name.`,
+            });
+        }
+
+        const author = await prisma.author.update({
+            where: { id },
+            data: { name },
+        });
+
+        res.json(author);
+    } catch {
+        next();
+    }
 });
 
-router.delete(':/id', (req, res) => {
     //deletes and author by a specified id
+router.delete(':/id', async (req, res, next) => {
+    try {
+        const id = +req.params.id;
+
+        const authExists = await prisma.author.findUnique({ where: { id }});
+
+        if (!authExists) {
+            return next({
+                status: 404,
+                message: `No author with id ${id} exists.`,
+            });
+        };
+
+        await prisma.author.delete({ where: { id }});
+
+        res.sendStatus(204);
+    } catch {
+        next();
+    }
 });
 
-router.get(':id/books', (req, res) => {
     //gets all books by specified author
-})
+router.get(':id/books', async (req, res, next) => {
+    try {
+        id = +req.params.id;
 
-router.post(':id/books', (req, res) => {
+        const author = await prisma.author.findUnique({ where: { id }});
+
+        if (!author) {
+            return next({
+                status: 404, 
+                message: `Could not find author with id ${id}.`,
+            });
+        }
+        
+        const books = await prisma.book.findMany({ where: {authorId: id }});
+
+        res.json(books);
+    } catch {
+        next();
+    }
+});
+
     //creates a new book in the req body for specified author
+router.post(':id/books', async (req, res, next) => {
+    try {
+        const id = +req.params.id;
+
+        const author = await prisma.author.findUnique({ where: { id }});
+
+        if (!author) {
+            return next({
+                status: 404, 
+                message: `Could not find author with id ${id}.`,
+            });
+        }
+
+        const { title } = req.body;
+
+        if (!title) {
+            return next( {
+                status: 400,
+                message: `Book must have a title.`,
+            });
+        }
+
+        const book = await prisma.book.create({ 
+            data: { title, author: { connect: { id }} }
+        });
+
+            res.json(book);
+    } catch {
+        next();
+    }
 });
--- api/books.js
+++ api/books.js
@@ -1,18 +1,92 @@
 const router = require('express').Router();
 module.exports = router;
 
-router.get('/', (req, res) => {
+const prisma = require('../prisma');
+
     // returns array of all books
+router.get('/', async (req, res, next) => {
+    try {
+        const books = await prisma.book.findMany();
+        res.json(books);
+    } catch {
+        next();
+    }
 });
 
-router.get('/:id', (req, res) => {
     // returns single book with specified id
+router.get('/:id', async (req, res, next) => {
+    try {
+        const id = +req.params.id;
+        const book = await prisma.book.findUnique({ where: { id }});
+        
+        if (!book) {
+            return next({
+                status: 404, 
+                message: `Could not find books by id ${id}.`
+            });
+        }
+        res.json(book);
+    } catch {
+        next();
+    }
 });
 
-router.put('/:id', (req, res) => {
     // overwrites a book with info from req body
+router.put('/:id', async (req, res, next) => {
+    try {
+        const id = +req.params.id;
+
+        const bookExists = await prisma.book.findUnique({ where: { id }});
+
+        if (!bookExists) {
+            return next({
+                status: 404, 
+                message: `No book exists with id ${id}`,
+            });
+        }
+
+        const { title, authorId } = req.body;
+        if (!title) {
+            return next({
+                status: 400, 
+                message: `Book must have a title.`,
+            });
+        } else if (!authorId) {
+            return next({
+                status: 400, 
+                message: `Book must have an authorId.`,
+            });
+        }
+
+        const book = await prisma.book.update({
+            where: { id }, 
+            data: { title, authorId },
+        });
+
+        res.json(book);
+    } catch {
+        next();
+    }
 });
 
-router.delete(':/id', (req, res) => {
     // deletes book with specified id
-})
+router.delete(':/id', async (req, res, next) => {
+    try {
+        const id = +req.params.id;
+
+        const bookExists = await prisma.book.findUnique({ where: { id }});
+
+        if (!bookExists) {
+            return next({
+                status: 404, 
+                message: `No book exists with id ${id}`,
+            });
+        }
+        
+        await prisma.book.delete({ where: { id }});
+
+        res.sendStatus(204);
+    } catch {
+        next();
+    }
+});
