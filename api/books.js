f016343397e99658a0986eb76ffe5988dcc8126b
CourtneyATX <courtney.mcguire.tx@gmail.com>
2023-11-01 13:32:56 -0500

    books routes outline

--- api/books.js
+++ api/books.js
@@ -1,2 +1,18 @@
 const router = require('express').Router();
 module.exports = router;
+
+router.get('/', (req, res) => {
+    // returns array of all books
+});
+
+router.get('/:id', (req, res) => {
+    // returns single book with specified id
+});
+
+router.put('/:id', (req, res) => {
+    // overwrites a book with info from req body
+});
+
+router.delete(':/id', (req, res) => {
+    // deletes book with specified id
+})
